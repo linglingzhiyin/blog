9b8e5d57e639a5c575b9bbc9f7c5b4f9526531a4
WangZhenjie1706 <1097531589@qq.com>
2018-12-25 16:32:14 +0800

    181225

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <Header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+     <h1>朾的个人博客</h1>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">主页</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="#">心得笔记</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="#">陌上花开</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="#">下载</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">关于</router-link>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto" >
+        <li class="nav-item">
+          <router-link class="nav-link" to="#">登陆</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="#">注册</router-link>
+        </li>
+      </ul>
+    </nav>
+  </Header>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>哈哈哈哈</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import Header from '@/components/Header.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // appHeader:Header
   }
 }
 </script>
