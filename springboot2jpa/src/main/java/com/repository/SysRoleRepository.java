d71f19acf5781878abeb59d4a4587383660e49b7
WangZhenjie1706 <1097531589@qq.com>
2019-01-01 17:44:58 +0800

    1911

---
+++ springboot2jpa/src/main/java/com/repository/SysRoleRepository.java
@@ -0,0 +1,7 @@
+package com.repository;
+
+import com.domain.SysRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SysRoleRepository extends JpaRepository<SysRole, Integer> {
+}
